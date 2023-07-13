00c062eabce454d7e01e9882204fe9a31931839c
Oliver Huang <olivershaohuang2006@gmail.com>
2023-07-13 14:08:37 -0400

    removed unneeded php file

--- send_email.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if ($_POST['message']) {
-        $subject = $_POST['subject'];
-        $email = $_POST['email'];
-        mail("ohuang4131@gmail.com", $subject, $_POST['message'], "From: $email");
-    }
-?>
-
